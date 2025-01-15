2fa04b0a75b04042086916707de54d861b197d8a
Khaw <150984271+KuNKhaw@users.noreply.github.com>
2025-01-15 15:00:30 +0700

    Delete Web directory

--- Web/script.js
+++
@@ -1,9 +0,0 @@
-function switchToRegister() {
-    document.getElementById('login-box').style.display = 'none';
-    document.getElementById('register-box').style.display = 'block';
-}
-
-function switchToLogin() {
-    document.getElementById('register-box').style.display = 'none';
-    document.getElementById('login-box').style.display = 'block';
-}
