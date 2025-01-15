355f1d2f2af92ef559ba212f732fa716fb2f04ba
Khaw <150984271+KuNKhaw@users.noreply.github.com>
2025-01-15 14:42:01 +0700

    Add files via upload

---
+++ Web/script.js
@@ -0,0 +1,9 @@
+function switchToRegister() {
+    document.getElementById('login-box').style.display = 'none';
+    document.getElementById('register-box').style.display = 'block';
+}
+
+function switchToLogin() {
+    document.getElementById('register-box').style.display = 'none';
+    document.getElementById('login-box').style.display = 'block';
+}
